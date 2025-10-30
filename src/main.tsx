46280d5af731675dc486359c2128d1c21c1cbfde
ZiloTech <uwamimcdonald@gmail.com>
2025-10-30 19:22:07 +0200

    Switch from BrowserRouter to HashRouter
    Replaces BrowserRouter with HashRouter in main.tsx to improve routing compatibility, likely for environments where server-side routing is not supported.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AuthProvider } from './auth'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
